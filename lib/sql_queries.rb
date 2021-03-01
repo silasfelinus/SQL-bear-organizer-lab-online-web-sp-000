89fe67305227c60794c2d4014bce13e5d0cba3af
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-01 03:23:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT name, age FROM bears WHERE gender == 'F'"
+  "SELECT * FROM bears WHERE id == 8"
 end
