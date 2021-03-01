c49df56b933cbb71cd31b9262f5bf1214920e2bf
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-01 03:19:34 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Kind", "Y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 11, "Female", "White", "Helpful", "Y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "Male", "Black", "Lovely",  "Y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "Female", "Lost", "Brown", "Y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 20, "Male", "Black", "Helpful", "Y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "Male", "Brown", "Angry", "N");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "Male", "Brown", "Irate", "Y");
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (7, "Male", "Brown", "Misunderstood", "N");
